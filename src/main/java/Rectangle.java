829def24775e12d461028735bcda9db1d81900ab
Brent Perry <burentoperii@gmail.com>
2016-12-28 16:07:41 -0600

    complete all tests and user interface.

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,6 +1,21 @@
 public class Rectangle {
+  private int mLength;
+  private int mWidth;
 
   public Rectangle(int length, int width) {
-    
+    mLength = length;
+    mWidth = width;
+  }
+
+  public boolean isSquare() {
+    return mLength == mWidth;
+  }
+
+  public int getLength() {
+    return mLength;
+  }
+
+  public int getWidth() {
+    return mWidth;
   }
 }
